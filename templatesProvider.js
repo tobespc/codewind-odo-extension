216655aa6bec243151003c79bb420b4bfd4db28b
jingfu wang <jingfu.j.wang@ibm.com>
2019-11-26 11:32:36 -0500

    Bind case
    Signed-off-by: jingfu wang <jingfu.j.wang@ibm.com>

--- templatesProvider.js
+++ templatesProvider.js
@@ -22,7 +22,7 @@ const CODEWIND_ODO_EXTENSION_BASE_PATH = '/codewind-workspace/.extensions/codewi
 const MASTER_INDEX_JSON_FILE = CODEWIND_ODO_EXTENSION_BASE_PATH + '/templates/master-index.json';
 const RECONCILED_INDEX_JSON_FILE = CODEWIND_ODO_EXTENSION_BASE_PATH + '/templates/index.json';
 const JSON_FILE_URL = 'file://' + RECONCILED_INDEX_JSON_FILE;
-const ODO_CATALOG_LIST_COMMAND = CODEWIND_ODO_EXTENSION_BASE_PATH + '/bin/odo catalog list components | tail -n+2';
+const ODO_CATALOG_LIST_COMMAND = CODEWIND_ODO_EXTENSION_BASE_PATH + '/bin/odo catalog list components -o json';
 
 
 module.exports = {
@@ -36,23 +36,24 @@ module.exports = {
         
                 const masterjson = JSON.parse(data);
 
-                // Run odo command to get list of catalog components available for cluster
+                // Run odo command to get list of catalog components available for cluster, then compare with mater index.json
+                // to generate updated index.json used for project creation
                 const odocomponents = [];
                 exec(ODO_CATALOG_LIST_COMMAND, (err, stdout) => {
                     if (err)
                         return reject(err);
-    
-                    stdout.split(os.EOL).forEach((line) => {
-                        const splitline = line.trim().split(/\s+/);
-                        let componentname = splitline[0];
-                        odocomponents.push(componentname);
-                    });
+
+                    const componentsjson = JSON.parse(stdout);
+
+                    for (const component of componentsjson.items) {
+                        odocomponents.push(component.metadata.name);
+                    }
 
                     // Loop through current list of templates in master index.json and delete any language 
                     // not in component list returned by odo command
                     // note: the master index.json is assumed to use same keywords for 'language' as odo uses for component 'name'
                     for(var i = 0; i < masterjson.length; i++) {
-                        if ( ! odocomponents.includes(masterjson[i].language) ) {
+                        if ( !odocomponents.includes(masterjson[i].language) ) {
                             masterjson.splice(i,1);
                         }
                     }
@@ -68,7 +69,7 @@ module.exports = {
                 });
             });
             
-            // Return a link to the updated json index file
+            // Return a link to the updated index.json index
             const repos = [];
             const projectStylesArr = [];
             projectStylesArr.push('OpenShift')
@@ -80,5 +81,55 @@ module.exports = {
             });
             resolve(repos);
         });
-    } 
+    },
+
+    getProjectTypes: async function() {
+        return new Promise((resolve, reject) => {
+            const projectTypes = [];
+
+            // Read master-index.json of currently defined templates for OpenShift
+            fs.readFile(MASTER_INDEX_JSON_FILE, 'utf8', function (err, data) {
+                if (err)
+                    return reject(err);
+        
+                const masterjson = JSON.parse(data);
+
+                // Run odo command to get list of catalog components available for cluster, then compare with mater index.json
+                // to generate supported language for project bind
+                const odocomponents = [];
+                exec(ODO_CATALOG_LIST_COMMAND, (err, stdout) => {
+                    if (err)
+                        return reject(err);
+    
+                    const componentsjson = JSON.parse(stdout);
+
+                    for (const component of componentsjson.items) {
+                        odocomponents.push(component.metadata.name);
+                    }
+
+                    // Loop through current list of templates in master index.json and add any language 
+                    // that in component list returned by odo command
+                    // note: the master index.json is assumed to use same keywords for 'language' as odo uses for component 'name'
+                    for(var i = 0; i < masterjson.length; i++) {
+                        if ( !odocomponents.includes(masterjson[i].language) ) {
+                            continue;
+                        } else {
+                            projectTypes.push({
+                                projectType: 'odo',
+                                projectSubtypes: {
+                                    label: 'OpenShift component',
+                                    items: [{
+                                        id: `OpenShift/${masterjson[i].language}`,
+                                        label: `OpenShift ${masterjson[i].language}`,
+                                        description: masterjson[i].description
+                                    }]
+                                }
+                            });
+                        }
+                    }
+                    resolve(projectTypes);
+                });
+            });
+        });
+    }
 }
