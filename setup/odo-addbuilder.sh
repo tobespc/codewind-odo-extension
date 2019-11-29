5eaa8c1fec8b278f718367d63d2f7d37ce411493
Len Theivendra <theivend@ca.ibm.com>
2019-11-29 11:18:03 -0500

    1115 - Add ODO Java component support and template
    Signed-off-by: Len Theivendra <theivend@ca.ibm.com>

---
+++ setup/odo-addbuilder.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+###################################################################################
+# Copyright (c) 2019 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+#
+# Contributors:
+#     IBM Corporation - initial API and implementation
+###################################################################################
+
+# Import OpenShift image stream
+oc import-image codewind-odo-openjdk18 --from=registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift --confirm
+
+# Tag the image as a builder
+oc annotate istag/codewind-odo-openjdk18:latest tags=builder
