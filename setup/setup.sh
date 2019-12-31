7e18019541af7c201a208f4e4be26d8cf4a55d70
jingfu wang <jingfu.j.wang@ibm.com>
2019-12-31 14:49:25 -0500

    Setup for hybrid case
    Signed-off-by: jingfu wang <jingfu.j.wang@ibm.com>

---
+++ setup/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+###################################################################################
+# Copyright (c) 2019 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+#
+# Contributors:
+#     IBM Corporation - initial API and implementation
+###################################################################################
+
+# Bind (rolebinding) additional cluster roles to Codewind service account(s)
+SERVICE_ACCOUNTS=$(kubectl get po --selector=app=codewind-pfe -o jsonpath='{.items[*].spec.containers[*].env[?(@.name=="SERVICE_ACCOUNT_NAME")].value}')
+for SERVICE_ACCOUNT in $SERVICE_ACCOUNTS; do
+    echo "Bind (rolebinding) additional cluster roles to Codewind service account: $SERVICE_ACCOUNT"
+    sed -i ' ' "s/<serviceaccount>/$SERVICE_ACCOUNT/g" codewind-odorolebinding.yaml
+    kubectl apply -f codewind-odoclusterrole.yaml
+    kubectl apply -f codewind-odorolebinding.yaml
+done
+
+# Import Java image stream to the cluster
+./odo-addbuilder.sh
